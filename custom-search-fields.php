8ba158c75b880223bedc64fc29bdd4634ba7ac1c
Andy Tran <hello@andytran.me>
2016-01-14 14:37:24 -0800

    Added in grunt task.

--- custom-search-fields.php
+++ custom-search-fields.php
@@ -16,7 +16,8 @@
 */
 
 function add_custom_search_fields_scripts() {
-  wp_enqueue_script( 'custom-search-fields', plugin_dir_url( __FILE__ ) . 'js/custom-search-fields.js', array( 'jquery' ), '0.0.1', true );
+  wp_enqueue_style( 'custom-search-fields', plugin_dir_url( __FILE__ ) . 'css/style.css' );
+  wp_enqueue_script( 'custom-search-fields', plugin_dir_url( __FILE__ ) . 'js/scripts.js', array( 'jquery' ), '0.0.1', true );
 }
 
 add_action( 'wp_enqueue_scripts', 'add_custom_search_fields_scripts' );
