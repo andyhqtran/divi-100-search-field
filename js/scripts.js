0ccf414dbfea38daa90422b0eb88c881768fcef9
Andy Tran <hello@andytran.me>
2016-01-14 15:19:07 -0800

    Removed old jquery events, added in click events.

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,12 @@ jQuery(document).ready(function ($) {
   $('.et_close_search_field').on('click', function () {
     $('.et_search_outer .et_search_form_container').removeClass('et_pb_search_visible');
   });
+
+  $('#et_search_icon').click(function () {
+    return false;
+  });
+
+  $('#et_search_icon').click(function () {
+    $('.et_search_form_container').addClass('et_pb_search_visible et_pb_no_animation');
+  });
 })
