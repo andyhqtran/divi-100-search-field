fc9f3e7d00266dc27efc834a248c268c43a6b4aa
Andy Tran <hello@andytran.me>
2016-01-14 16:14:20 -0800

    Added in custom search field events.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,31 @@
 jQuery(document).ready(function ($) {
-  $('header .et_search_outer').clone().addClass('et_search_outer_cloned et_search_outer_style_1').appendTo('body');
 
-  $('header .et_search_outer').remove();
+  // Appends current search to body
+  // Replaces .et_search_outer with .et_custom_search
+  // Removes unnessary div
+  $(".et_search_outer")
+    .insertAfter("#page-container")
+    .removeClass("et_search_outer")
+    .addClass("et_custom_search")
+    .html($(".et-search-form"));
 
-  $('.et_close_search_field').on('click', function () {
-    $('.et_search_outer .et_search_form_container').removeClass('et_pb_search_visible');
+  // Appends close button to search
+  $(".et_custom_search").prepend("<span class='et_custom_search_close'>x</span>")
+
+  // Close button removes visible class and adds hide class
+  $(".et_custom_search").on("click", ".et_custom_search_close", function () {
+    $(".et_custom_search").removeClass("et_custom_search_visible").addClass(".et_custom_search_hide");
   });
 
-  $('#et_search_icon').click(function () {
+  // Disables default click events
+  $("#et_search_icon").click(function () {
     return false;
   });
 
-  $('#et_search_icon').click(function () {
-    $('.et_search_form_container').addClass('et_pb_search_visible et_pb_no_animation');
+  // Search icon removes hide class and adds visible class
+  $("#et_search_icon").click(function () {
+    $(".et_custom_search")
+      .removeClass("et_custom_search_hide")
+      .addClass("et_custom_search_visible");
   });
 })
