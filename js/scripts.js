4b9616cfa2a2a1110a225e11d7ec1b7ffbed7e47
Andy Tran <hello@andytran.me>
2016-01-21 12:41:04 -0800

    Organized scss structure, updated prefixes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
 jQuery(document).ready(function ($) {
 
   // Appends current search to body
-  // Replaces .et_search_outer with .et_custom_search
+  // Replaces .et_search_outer with .et_divi_100_custom_search_fields
   // Removes unnessary div
   $(".et_search_outer")
     .insertAfter("#page-container")
     .removeClass("et_search_outer")
-    .addClass("et_custom_search et_custom_search_hide")
+    .addClass("et_divi_100_custom_search_fields et_divi_100_custom_search_fields--hide")
     .html($(".et-search-form"));
 
   // Appends close button to search
-  $(".et_custom_search").prepend("<span class='et_custom_search_close'></span>")
+  $(".et_divi_100_custom_search_fields").prepend("<span class='et_divi_100_custom_search_fields__close'></span>")
 
   // Close button removes visible class and adds hide class
-  $(".et_custom_search").on("click", ".et_custom_search_close", function () {
-    $(".et_custom_search").removeClass("et_custom_search_visible").addClass("et_custom_search_hide");
+  $(".et_divi_100_custom_search_fields").on("click", ".et_divi_100_custom_search_fields__close", function () {
+    $(".et_divi_100_custom_search_fields").removeClass("et_divi_100_custom_search_fields--visible").addClass("et_divi_100_custom_search_fields--hide");
   });
 
   // Disables default click events
@@ -24,8 +24,8 @@ jQuery(document).ready(function ($) {
 
   // Search icon removes hide class and adds visible class
   $("#et_search_icon").click(function () {
-    $(".et_custom_search")
-      .removeClass("et_custom_search_hide")
-      .addClass("et_custom_search_visible");
+    $(".et_divi_100_custom_search_fields")
+      .removeClass("et_divi_100_custom_search_fields--hide")
+      .addClass("et_divi_100_custom_search_fields--visible");
   });
 })
